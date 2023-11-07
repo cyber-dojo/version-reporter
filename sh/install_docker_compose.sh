a51304b4f19851836786f77f4fd344efb4037a46
JonJagger <jon@kosli.com>
2023-11-07 20:45:04 +0000

    CI: install docker

--- sh/install_docker_compose.sh
+++ sh/install_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeu
 
+apt-get update && apt-get install -y docker.io
 curl -L "https://github.com/docker/compose/releases/download/`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | sed 's#.*tag/##g' && echo`/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
-
