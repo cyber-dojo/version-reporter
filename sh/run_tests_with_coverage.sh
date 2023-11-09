0963dfe6893a045a39389cad52de0866f4fa5008
zhelezovartem <zhelezovartem@gmail.com>
2023-11-09 17:21:44 +0600

    enable branch-coverage and security scan jobs

--- sh/run_tests_with_coverage.sh
+++ sh/run_tests_with_coverage.sh
@@ -8,11 +8,11 @@ source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/echo_versioner_env_vars.sh"
 source "${SH_DIR}/lib.sh"
-source "${SH_DIR}/on_ci_upgrade_docker_compose.sh"
+# source "${SH_DIR}/on_ci_upgrade_docker_compose.sh"
 source "${SH_DIR}/test_in_containers.sh"
 export $(echo_versioner_env_vars)
 
-on_ci_upgrade_docker_compose
+# on_ci_upgrade_docker_compose
 server_up_healthy_and_clean
 client_up_healthy_and_clean "$@"
 test_in_containers "$@"
